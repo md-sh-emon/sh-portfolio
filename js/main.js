be29913745d53879f474aabf6e7c597aae1086a5
SH EMON <mohammadsaeed9195@gmail.com>
2021-04-13 13:14:14 +0600

    added highlight active item on portfolio menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 jQuery(document).ready(function(){
+    jQuery('.portfolio-menu').on('click', 'li', function(){
+        jQuery(this).addClass('active').siblings().removeClass('active');
+    });
     var mixer = mixitup('.portfolio-gallery-container');
 });
